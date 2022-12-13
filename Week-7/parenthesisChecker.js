3e9f3da93f096f30d98b7638d7877370c94570f5
Vinay Nandal <vinaynandal0008@gmail.com>
2022-12-13 20:31:16 +0530

    parenthesis checker

---
+++ Week-7/parenthesisChecker.js
@@ -0,0 +1,59 @@
+
+function areBracketsBalanced(expr)
+{
+     
+    let stack = [];
+ 
+    // Traversing the Expression
+    for(let i = 0; i < expr.length; i++)
+    {
+        let x = expr[i];
+ 
+        if (x == '(' || x == '[' || x == '{')
+        {
+             
+            // Push the element in the stack
+            stack.push(x);
+            continue;
+        }
+ 
+    
+        if (stack.length == 0)
+            return false;
+             
+        let check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+ 
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+ 
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+ 
+    // Check Empty Stack
+    return (stack.length == 0);
+}
+ 
+let expr = "([{}])";
+ 
+//call function
+if (areBracketsBalanced(expr))
+    console.log("Balanced ");
+else
+    console.log("Not Balanced ");
+
+// Time Complexity: O(N)
+// Auxiliary Space: O(N)
